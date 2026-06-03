43d49cac1fb374699baa0b6f79315c9a7db49f26
Zaid Huaquisto <zaidhuaquisto@gmail.com>
2026-06-02 22:14:36 -0500

    Para aprender

---
+++ CURSO_JAVAS/codigo.js
@@ -0,0 +1,14 @@
+// let numero;
+//numero = 29
+//alert(numero)
+
+
+/*Bueno me quede en el minuto 40*/
+
+// prompt("Ingrese su nombre");           //Para texto
+
+let nombre = prompt("Ingrese su nombre"); //Para texto
+
+alert("hola " + nombre); 
+
+document.write(nombre); //Escribe en el documento HTML 
